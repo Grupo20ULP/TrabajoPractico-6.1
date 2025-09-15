04c3036dcdd8c80e747a7bb988b114c4c64ad3ba
Heber Gomez <heber12398@gmail.com>
2025-09-15 19:55:05 -0300

    tp-6.1

---
+++ src/trabajo/practico/pkg6/pkg1/TrabajoPractico61.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajo.practico.pkg6.pkg1;
+
+/**
+ *
+ * @author Heber Gomez
+ */
+public class TrabajoPractico61 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main (String[] args) {
+        // TODO code application logic here
+    }
+    
+}
